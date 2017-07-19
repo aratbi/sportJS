acf21d90e651fe8152a05d2a306376301f78817b
RATBI Ayoub <ayoub.ratbi@soprasteria.com>
2017-07-19 11:48:47 +0200

    correction exo templating

--- src/app/plan/program/exercices-list/exercices-list.component.ts
+++ src/app/plan/program/exercices-list/exercices-list.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, Output} from '@angular/core';
 import {Exercice} from "../../../shared/program/exercice";
+import {forEach} from "@angular/router/src/utils/collection";
 
 @Component({
   selector: 'sp-exercices-list',
@@ -9,8 +10,43 @@ import {Exercice} from "../../../shared/program/exercice";
 export class ExercicesListComponent implements OnInit {
 
   @Input() exercices: Exercice[];
+  selectedExercices: Exercice[] = [];
 
-  constructor() { }
+  constructor() {
+  }
 
-  ngOnInit() { }
+  ngOnInit() {
+  }
+
+  deplaceUp(index: number, e): void {
+    e.stopImmediatePropagation();
+    const inter = this.exercices[index];
+    this.exercices[index] = this.exercices[index - 1];
+    this.exercices[index - 1] = inter;
+  }
+
+  deplaceDown(index: number, e): void {
+    e.stopImmediatePropagation();
+    const inter = this.exercices[index];
+    this.exercices[index] = this.exercices[index + 1];
+    this.exercices[index + 1] = inter;
+  }
+
+  deleteEx(index: number): void {
+    this.exercices.splice(index, 1);
+  }
+
+  selectEx(exercice: Exercice): void {
+    if (this.selectedExercices.indexOf(exercice) === -1) {
+      this.selectedExercices.push(exercice);
+    } else {
+      this.selectedExercices.splice(this.selectedExercices.indexOf(exercice));
+    }
+  }
+
+  deleteAllEx(): void {
+    this.selectedExercices.forEach(ex => this.exercices.splice(this.exercices.indexOf(ex), 1));
+    this.selectedExercices.splice(0);
+  }
+
 }
